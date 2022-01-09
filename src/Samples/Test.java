3a2f946d25669b4106fb74aadf27de4ec14eec9b
CSWesley <booked1234512@gmail.com>
2022-01-09 13:24:03 -0800

    Beginning testing for translation

---
+++ src/Samples/Test.java
@@ -0,0 +1,5 @@
+// test
+
+public class Test {
+
+}
--- src/Translator/Translate.java
+++ src/Translator/Translate.java
@@ -2,7 +2,7 @@ package Translator;
 
 import java.io.IOException;
 import java.util.Scanner;
-import Translator.Translate;
+import java.io.File;
 
 public class Translate {
 
@@ -24,23 +24,27 @@ public class Translate {
     }
 
     private void translate(String inputFile, String outputDir) throws IOException {
-        String fullInput = readFile(inputFile);
+        File file = new File(inputFile);
+        String fullInput = readFile(file);
         System.out.println(fullInput);
     }
 
-    private String readFile(String inputFile) {
+    private String readFile(File inputFile) throws IOException {
         Scanner scanner = new Scanner(inputFile);
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             if (line.startsWith("//")) {
                 // go to next line
+                System.out.println("Skipping comment: " + line);
                 continue;
             } else if (line.startsWith("public class") || line.startsWith("class")) {
                 // get the class name
                 String className = line.substring(line.indexOf("class") + 6, line.indexOf("{"));
-                return className;
+                System.out.println("Class name: " + className);
+                continue;
             } else {
                 // translate the line
+                System.out.println("Translating line: " + line + " (Skipped)");
                 continue;
             }
         }
